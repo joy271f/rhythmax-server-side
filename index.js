ddbbd699ce411ee181d1b3dff77ede6f05e259e6
joychandradas <joychandradas007@gmail.com>
2023-06-14 14:56:42 +0600

    create get api for all and single id

--- index.js
+++ index.js
@@ -1,7 +1,7 @@
 const express = require("express");
 const cors = require("cors");
-const { MongoClient, ServerApiVersion } = require("mongodb");
-require('dotenv').config()
+const { MongoClient, ServerApiVersion, ObjectId } = require("mongodb");
+require("dotenv").config();
 const app = express();
 const port = process.env.PORT || 5000;
 
@@ -11,8 +11,7 @@ app.use(express.json());
 
 // mongoDB
 
-const uri =
-  `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASS}@cluster0.krjt8gu.mongodb.net/?retryWrites=true&w=majority`;
+const uri = `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASS}@cluster0.krjt8gu.mongodb.net/?retryWrites=true&w=majority`;
 
 // Create a MongoClient with a MongoClientOptions object to set the Stable API version
 const client = new MongoClient(uri, {
@@ -27,16 +26,25 @@ async function run() {
   try {
     // Connect the client to the server	(optional starting in v4.7)
     const classCollection = client.db("rhythmaxDB").collection("classes");
-    
 
     // class
-    app.post('/class' , async(req, res) => {
-        const classes = req.body;
-        console.log('Class', classes);
-        const result = await classCollection.insertOne(classes);
-        res.send(result);
-    })
+    app.get("/classes/:id", async (req, res) => {
+      const id = req.params.id;
+      const query = { _id: new ObjectId(id) };
+      const result = await classCollection.findOne(query);
+      res.send(result);
+    });
 
+    app.get("/classes", async (req, res) => {
+      const result = await classCollection.find().toArray();
+      res.send(result);
+    });
+
+    app.post("/class", async (req, res) => {
+      const classes = req.body;
+      const result = await classCollection.insertOne(classes);
+      res.send(result);
+    });
 
     // Send a ping to confirm a successful connection
     await client.db("admin").command({ ping: 1 });
